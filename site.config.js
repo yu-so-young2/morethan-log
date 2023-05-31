615e67238142a75bdb7854c293e29d354c13610c
yu-so-young <sy980911@gmail.com>
2023-05-31 15:00:50 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "soyoung",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "Backend Developer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
