9a09d25624d08d2dcab810f8e015251d3e4e4367
yu-so-young <sy980911@gmail.com>
2023-06-02 13:15:46 +0900

    Update PostHeader.tsx

--- src/containers/Detail/components/PostDetail/PostHeader.tsx
+++ src/containers/Detail/components/PostDetail/PostHeader.tsx
@@ -54,7 +54,7 @@ const PostHeader: React.FC<Props> = ({ data }) => {
               <Image
                 src={data.thumbnail}
                 className="object-cover"
-                style={{ object-fit: "contain" }}
+                style={{ objectFit: "contain" }}
                 layout="fill"
                 alt={data.title}
               />
